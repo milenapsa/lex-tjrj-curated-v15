9037dca143cd26af2c835a0233269142d2369730
milenapsa <milena@peterle.adv.br>
2026-07-11 21:25:04 -0300

    fix: bump TJRJ connector after summary segmentation correction

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 PORT=int(os.getenv("PORT","8080"))
 UPSTREAM=os.getenv("LEX_UPSTREAM","http://homosapiens-lex-tjmg-curated-v14:8080")
-VERSION="0.15.1-tjrj-curated"
-UA="Lex-HomoSapiens/0.15.1"
+VERSION="0.15.2-tjrj-curated"
+UA="Lex-HomoSapiens/0.15.2"
 TTL=1800
 SUMULAS="https://portaltj.tjrj.jus.br/documents/d/portal-conhecimento/sumulas-2026"
 CANCELADAS="https://portaltj.tjrj.jus.br/documents/d/portal-conhecimento/sumulas-canceladas"
